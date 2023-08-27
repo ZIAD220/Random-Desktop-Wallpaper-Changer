e574c99edddc561cccde42c6df1bb03c1bed012b
ZIAD220 <ziad.beda95@gmail.com>
2023-08-27 16:03:37 +0300

    Finialized the code and adjusted the path directory.

--- controller.py
+++ controller.py
@@ -1,3 +1,4 @@
+import os.path
 import random
 import ctypes
 import requests
@@ -8,51 +9,50 @@ def flow(query):
     # Modifying the query to replace %20 instead of spaces.
     new_query = query.replace(" ", "%20")
 
-    print(new_query)
-
     page = requests.get(f"https://wallpaperswide.com/search.html?q={new_query}")
+
     page = get_random_page(page, query)
     if page is None:
         return False
+
     page = get_random_wallpaper(page)
+
     download_wallpaper(page)
+
     set_wallpaper()
+
     return True
 
-# Get random picture.
+
 def get_random_page(page, query):
     src = page.content
     soup = bs4.BeautifulSoup(src, 'lxml')
 
-    # Getting number of pages available.
+    # Handling corner case when there's no results at all.
     pagination = soup.find('div', {'class': 'pagination'})
     if pagination is None:
         return None
 
+    # Getting number of pages available.
     max_page = int(pagination.contents[len(pagination) - 2].text)
 
     random_page = random.randint(1, max_page)
     page = requests.get(f'https://wallpaperswide.com/search/page/{random_page}?q={query}')
+
     src = page.content
     soup = bs4.BeautifulSoup(src, 'lxml')
     return soup
 
 
-
-
-# Get random wallpaper.
 def get_random_wallpaper(page):
     wallpapers = page.find_all('li', {'class': 'wall'})
-    print(wallpapers)
+
     random_index = random.randint(0, len(wallpapers) - 1)
     random_wallpaper = 'https://wallpaperswide.com' + wallpapers[random_index].find('a')['href']
-    print(random_wallpaper)
+
     return random_wallpaper
 
 
-
-
-# Download wallpaper
 def download_wallpaper(url):
     page = requests.get(url)
     src = page.content
@@ -65,17 +65,16 @@ def download_wallpaper(url):
         if a['href'].find('1920x1080') > -1:
             image_link += a['href']
 
-    print(image_link)
     image = requests.get(image_link)
     with open("image.jpg", "wb") as f:
         f.write(image.content)
 
 
-
-
 # Setting the downloaded image to be the desktop wallpaper.
 def set_wallpaper():
     SPI_SETDESKWALLPAPER = 20
-    ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, "E:\Codes\Web Scraping\image.jpg", 3)
+    ROOT = os.path.dirname(os.path.abspath(__file__))
+    path = ROOT + "\image.jpg"
+    ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, path, 3)
     return
 
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from time import sleep
+
 
 class GUI:
     def __init__(self):
@@ -8,7 +8,9 @@ class GUI:
         self.root.title("Random Desktop Wallpaper Changer")
         self.root.configure(background="black")
 
-        self.topicLabel = tk.Label(self.root, text="Search for a wallpaper", font=('Arial', 20), bg='black', fg='white')
+        self.topicLabel = tk.Label(self.root,
+                                   text="Search for a wallpaper",
+                                   font=('Arial', 20), bg='black', fg='white')
         self.topicLabel.pack(padx=20, pady=20)
 
         self.topicBox = tk.Text(self.root, height=1, font=('Arial', 16))
@@ -22,7 +24,7 @@ class GUI:
                                 background="#4285F4")
         self.button.pack()
 
-        self.status_label = tk.Label(self.root, text="Please wait.", font=('Arial', 16), bg='black', fg='white')
+        self.status_label = tk.Label(self.root, text="", font=('Arial', 16), bg='black', fg='white')
         self.status_label.pack_forget()
 
         self.githubLabel = tk.Label(self.root, text="github.com/ZIAD220", font=('Arial', 11), bg='black', fg='white')
@@ -33,13 +35,12 @@ class GUI:
 
     def handle_input(self):
         topic = self.topicBox.get("1.0", tk.END)
-
         if topic == "":
             return
-        self.status_label.config(text="Please wait.")
-        self.status_label.pack(padx=20, pady=20)
+
         from controller import flow
         found = flow(topic)
+
         if not found:
             self.status_label.config(text="No Results found.", fg="red")
             self.status_label.pack(padx=20, pady=20)
@@ -52,8 +53,8 @@ class GUI:
             self.handle_input()
 
 
-# Here we handle input.
 def init():
     GUI()
+
 
 init()
