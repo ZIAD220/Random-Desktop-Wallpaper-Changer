444715cbc231120c33275056b50c2128f9fe5d37
ZIAD220 <ziad.beda95@gmail.com>
2023-08-27 12:54:45 +0300

    Connected the gui with the program.

---
+++ controller.py
@@ -0,0 +1,74 @@
+import random
+import ctypes
+import requests
+import bs4
+
+
+def flow(query):
+    # Modifying the query to replace %20 instead of spaces.
+    new_query = query.replace(" ", "%20")
+
+    print(new_query)
+
+    page = requests.get(f"https://wallpaperswide.com/search.html?q={new_query}")
+    page = get_random_page(page, query)
+    page = get_random_wallpaper(page)
+    download_wallpaper(page)
+    set_wallpaper()
+
+# Get random picture.
+def get_random_page(page, query):
+    src = page.content
+    soup = bs4.BeautifulSoup(src, 'lxml')
+
+    # Getting number of pages available.
+    max_page = int(soup.find('div', {'class': 'pagination'}).contents[4].text)
+
+    random_page = random.randint(1, max_page)
+    page = requests.get(f'https://wallpaperswide.com/search/page/{random_page}?q={query}')
+    src = page.content
+    soup = bs4.BeautifulSoup(src, 'lxml')
+    return soup
+
+
+
+
+# Get random wallpaper.
+def get_random_wallpaper(page):
+    wallpapers = page.find_all('li', {'class': 'wall'})
+    print(wallpapers)
+    random_index = random.randint(0, len(wallpapers) - 1)
+    random_wallpaper = 'https://wallpaperswide.com' + wallpapers[random_index].find('a')['href']
+    print(random_wallpaper)
+    return random_wallpaper
+
+
+
+
+# Download wallpaper
+def download_wallpaper(url):
+    page = requests.get(url)
+    src = page.content
+    soup = bs4.BeautifulSoup(src, 'lxml')
+
+    resolutions = soup.findAll('a', {'target': '_self'})
+
+    image_link = 'https://wallpaperswide.com'
+    for a in resolutions:
+        if a['href'].find('1920x1080') > -1:
+            image_link += a['href']
+
+    print(image_link)
+    image = requests.get(image_link)
+    with open("image.jpg", "wb") as f:
+        f.write(image.content)
+
+
+
+
+# Setting the downloaded image to be the desktop wallpaper.
+def set_wallpaper():
+    SPI_SETDESKWALLPAPER = 20
+    ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, "E:\Codes\Web Scraping\image.jpg", 3)
+    return
+
--- main.py
+++ main.py
@@ -1,71 +1,44 @@
-import random
-import ctypes
-import requests
-import bs4
+import tkinter as tk
 
-# Here we need to handle input.
+class GUI:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.geometry("360x280")
+        self.root.title("Random Desktop Wallpaper Changer")
+        self.root.configure(background="black")
 
+        self.topicLabel = tk.Label(self.root, text="Search for a wallpaper", font=('Arial', 20), bg='black', fg='white')
+        self.topicLabel.pack(padx=20, pady=20)
 
-page = requests.get("https://wallpaperswide.com/search.html?q=assassins%20creed")
+        self.topicBox = tk.Text(self.root, height=1, font=('Arial', 16))
+        self.topicBox.bind("<KeyPress>", self.enter_key)
+        self.topicBox.pack(padx=10, pady=20)
 
+        self.button = tk.Button(self.root,
+                                text="Get Wallpaper!",
+                                font=('Arial', 20),
+                                command=self.handle_input,
+                                background="#4285F4")
+        self.button.pack()
 
-# Get random picture.
-def get_random_page(page):
-    src = page.content
-    soup = bs4.BeautifulSoup(src, 'lxml')
+        self.githubLabel = tk.Label(self.root, text="github.com/ZIAD220", font=('Arial', 11), bg='black', fg='white')
+        self.githubLabel.pack(side='bottom')
+        self.githubLabel.pack(side='right')
 
-    # Getting number of pages available.
-    max_page = int(soup.find('div', {'class': 'pagination'}).contents[4].text)
+        self.root.mainloop()
 
-    random_page = random.randint(1, max_page)
-    page = requests.get(f'https://wallpaperswide.com/search/page/{random_page}?q=assassins%20creed')
-    src = page.content
-    soup = bs4.BeautifulSoup(src, 'lxml')
-    return soup
+    def handle_input(self):
+        topic = self.topicBox.get("1.0", tk.END)
+        from controller import flow
+        flow(topic)
 
-
-page = get_random_page(page)
-
-
-# Get random wallpaper.
-def get_random_wallpaper(page):
-    wallpapers = page.find_all('li', {'class': 'wall'})
-    print(wallpapers)
-    random_index = random.randint(0, len(wallpapers) - 1)
-    random_wallpaper = 'https://wallpaperswide.com' + wallpapers[random_index].find('a')['href']
-    print(random_wallpaper)
-    return random_wallpaper
-
-
-page = get_random_wallpaper(page)
+    def enter_key(self, event):
+        if event.keysym == "Return":
+            self.handle_input()
 
 
-# Download wallpaper
-def download_wallpaper(url):
-    page = requests.get(url)
-    src = page.content
-    soup = bs4.BeautifulSoup(src, 'lxml')
+# Here we handle input.
+def init():
+    GUI()
 
-    resolutions = soup.findAll('a', {'target': '_self'})
-
-    image_link = 'https://wallpaperswide.com'
-    for a in resolutions:
-        if a['href'].find('1920x1080') > -1:
-            image_link += a['href']
-
-    print(image_link)
-    image = requests.get(image_link)
-    with open("image.jpg", "wb") as f:
-        f.write(image.content)
-
-
-download_wallpaper(page)
-
-
-# Setting the downloaded image to be the desktop wallpaper.
-def set_wallpaper():
-    SPI_SETDESKWALLPAPER = 20
-    ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, "E:\Codes\Web Scraping\image.jpg", 3)
-    return
-
-set_wallpaper()
+init()
